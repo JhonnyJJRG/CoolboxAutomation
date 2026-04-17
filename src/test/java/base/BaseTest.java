323d9e3dd414755d8b6448a0ac623deb80289fe8
JhonnyJJRG <jhonnyjesus19@gmail.com>
2026-04-16 23:31:54 -0500

    BaseTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,33 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
 public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        // Para cambiar a Edge, solo usa: WebDriverManager.edgedriver().setup(); driver = new EdgeDriver();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        // Espera dinámica: clave para el nivel Sobresaliente
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.coolbox.pe");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
